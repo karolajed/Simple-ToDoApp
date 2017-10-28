7eaa7b886d3eacc4b29899ab4586ea25078355c5
Karola <karola.jedrychowska@gmail.com>
2017-10-28 19:40:08 +0200

    shortering the code

--- addTask.js
+++ addTask.js
@@ -7,20 +7,21 @@ function Task(description, state) {
 }
 Task.UID = 1;
 
+
 const addButton = document.getElementById('add-btn');
 addButton.addEventListener('click', addTask);
-
-
 
 let newTaskTyping = new Task();
 
 function addTask() {
   newTaskTyping.description = document.querySelector('#task').value;
-  
+
   newTaskTyping.state = document.querySelector('input[name="urgency"]:checked').value;
 
-  var html = (newTaskTyping.state == 'im' ? (`<div class="panel panel-default todo-item">
-<div class="panel-body alert alert-danger">
+  var alert = (newTaskTyping.state == 'im' ? "alert-danger" : (newTaskTyping.state == 'ur' ? "alert-warning" : "alert-info"));
+
+  var task = `<div class="panel panel-default todo-item">
+<div class="panel-body alert ` + alert + `">
 <div class="row">
 <div class="col-sm-8">
 <p class="todo-description">
@@ -35,61 +36,15 @@ ${newTaskTyping.description}
 </div>
 </div>
 <div class="col-sm-2">
-<button type="submit" class="btn btn-block btn-danger">
+<button type="submit" class="btn btn-block btn-danger" id="delete` + newTaskTyping.id +`">
 Usuń
 </button>
 </div>
 </div>
 </div>
-</div>`) : (newTaskTyping.state == 'ur' ? (`<div class="panel panel-default todo-item">
-<div class="panel-body alert alert-warning">
-<div class="row">
-<div class="col-sm-8">
-<p class="todo-description">
-${newTaskTyping.description}
-</p>
-</div>
-<div class="col-sm-2">
-<div class="checkbox">
-<label>
-<input type="checkbox"> Gotowe
-</label>
-</div>
-</div>
-<div class="col-sm-2">
-<button type="submit" class="btn btn-block btn-danger">
-Usuń
-</button>
-</div>
-</div>
-</div>
-</div>`) : (`<div class="panel panel-default todo-item">
-<div class="panel-body alert alert-info">
-<div class="row">
-<div class="col-sm-8">
-<p class="todo-description">
-${newTaskTyping.description}
-</p>
-</div>
-<div class="col-sm-2">
-<div class="checkbox">
-<label>
-<input type="checkbox"> Gotowe
-</label>
-</div>
-</div>
-<div class="col-sm-2">
-<button type="submit" class="btn btn-block btn-danger">
-Usuń
-</button>
-</div>
-</div>
-</div>
-</div>`)))
-  
-  
-  
+</div>`;
+
   var todoPanel = document.querySelector('.todo-panel');
-  todoPanel.innerHTML += html;
+  todoPanel.innerHTML += task;
 }
 
